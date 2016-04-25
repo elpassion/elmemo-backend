2fbca94958ef2967f6929e579a92c63f07d634fe
Karol Wyliziński <k.wylizinski@wp.pl>
2016-04-25 14:55:01 +0200

    revealing requested fields and check if they match

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,17 +2,43 @@ require_relative '../../lib/game/board'
 class GameController < ApplicationController
 
   def create
-    @game = Game.create(state: { 'board': ::Board.new })
+    @game = Game.create(state: {'board': ::Board.new})
     redirect_to game_show_path(@game.id)
   end
 
   def show
     @game = Game.find(params[:id])
-    game = @game.dup
-    puts game.state[:board].inspect
+    field1 = params[:field1]
+    field2 = params[:field2]
+    check_if_pair(field1, field2, @game) if field1 && field2
+    game = render_for_no_params(@game.dup)
+    game = reveal([field1, field2], @game.dup) if field1 || field2
+
+    render json: game
+  end
+
+  private
+
+  def reveal(chosen_fields, game)
     game.state[:board].fields.each do |field|
-      field.value = 'unknown'
+      field.value = 'unknown' unless chosen_fields.include?(field.id) || field.matched
     end
-    render json: game[:state]
+  end
+
+  def check_if_pair(field1, field2, game)
+    first_field = game.state[:board].fields.select { |field| field.id == field1 }.first
+    second_field = game.state[:board].fields.select { |field| field.id == field2 }.first
+
+    if first_field.value == second_field.value
+      first_field.matched = true
+      second_field.matched = true
+    end
+    game.save
+  end
+
+  def render_for_no_params(game)
+    game.state[:board].fields.each do |field|
+      field.value = 'unknown' unless field.matched
+    end
   end
 end
--- lib/game/field.rb
+++ lib/game/field.rb
@@ -1,9 +1,10 @@
 class Field
 
-  attr_accessor :value
+  attr_accessor :value, :matched, :id
 
   def initialize(value)
     @id = SecureRandom.uuid
     @value = value
+    @matched = false
   end
 end
